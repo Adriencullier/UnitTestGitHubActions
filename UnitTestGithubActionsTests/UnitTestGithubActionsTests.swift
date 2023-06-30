d0ffe13d763da128edfd1f4ad25d629ba73fb66c
Adrien Cullier <adrien.cullier@zenchef.com>
2023-06-30 14:37:03 +0200

    wip: update yml and add fail test

--- UnitTestGithubActionsTests/UnitTestGithubActionsTests.swift
+++ UnitTestGithubActionsTests/UnitTestGithubActionsTests.swift
@@ -15,6 +15,7 @@ final class UnitTestGithubActionsTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
+        XCTAssert(false)
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
