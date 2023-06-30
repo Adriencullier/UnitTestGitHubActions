f2bb26256ef29dcd1d5202792f0ea91781a26dcb
Adrien Cullier <adrien.cullier@zenchef.com>
2023-06-30 14:49:01 +0200

    wip: test true

--- UnitTestGithubActionsTests/UnitTestGithubActionsTests.swift
+++ UnitTestGithubActionsTests/UnitTestGithubActionsTests.swift
@@ -15,7 +15,7 @@ final class UnitTestGithubActionsTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        XCTAssert(false)
+        XCTAssert(true)
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
