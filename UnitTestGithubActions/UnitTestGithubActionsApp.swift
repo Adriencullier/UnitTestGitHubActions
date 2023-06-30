52c8e44bba0e11786026b8f4a203b7dd4d92e88d
Adrien Cullier <adrien.cullier@zenchef.com>
2023-06-30 13:55:11 +0200

    Initial Commit

---
+++ UnitTestGithubActions/UnitTestGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestGithubActionsApp.swift
+//  UnitTestGithubActions
+//
+//  Created by Adrien Cullier on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
